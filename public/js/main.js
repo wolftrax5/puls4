6c9cc9474c2227ad67f312aaea155a727e1402f9
Siedrix <Siedrix@gmail.com>
2013-11-06 14:42:58 -0500

    Data layer and UI synced

--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,14 @@ $(document).ready(function(){
 	// c = new Puls3.Collections.Articles()
 	window.collections.articles = new Puls3.Collections.Articles();
 
+	window.collections.articles.on('add', function (model) {
+		// Agregar nuevas vistas de articulos aqui
+		var view = new Puls3.Views.Article({model: model});
+
+		view.render();
+
+		view.$el.appendTo('.posts');
+	});
+
 	window.collections.articles.fetch();
 });
