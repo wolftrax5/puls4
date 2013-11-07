669f8ebbfd1788b7b0c89ce4e03c33827dceac42
Siedrix <Siedrix@gmail.com>
2013-11-07 13:31:50 -0500

    Added upvote and downvote

--- public/js/backbone/views/article-view.js
+++ public/js/backbone/views/article-view.js
@@ -1,11 +1,31 @@
 Puls3.Views.Article = Backbone.View.extend({
+	events:{
+		"click .votos .up" : "upvote",
+		"click .votos .down" : "downvote"
+	},
 	tagName : "article",
 	className : "post",
 	initialize : function () {
-		// debugger;
+		var self = this;
+
+		this.model.on('change', function (data) {
+			self.render();
+		});
 
 		this.template = _.template( $('#article-template').html() );
 		// this.template = swig.compile( $('#article-template').html() );
+	},
+	upvote : function () {
+		var votes = parseInt( this.model.get('votes'), 10 );
+
+		this.model.set('votes', ++votes);
+		this.model.save();
+	},
+	downvote : function () {
+		var votes = parseInt( this.model.get('votes'), 10 );
+
+		this.model.set('votes', --votes);
+		this.model.save();
 	},
 	render : function () {
 		var data = this.model.toJSON();
